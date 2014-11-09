ff1e5f4cf7435c702b6f27033b7a748905e713e6
Jean-Marc Lagacé <jean-marc@m2i3.com>
2014-11-09 10:33:26 -0500

    automated update

---
+++ mysite/javascript/menu.js
@@ -0,0 +1,17 @@
+function addMega(){ 
+	$(this).addClass("hovering"); 
+} 
+ 
+function removeMega(){ 
+	$(this).removeClass("hovering"); 
+}
+
+var megaConfig = {     
+	interval: 500, 
+	sensitivity: 4,
+	over: addMega, 
+	timeout: 500, 
+	out: removeMega 
+};
+
+$("li.mega").hoverIntent(megaConfig);
